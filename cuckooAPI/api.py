6be9ff5ae726d8c16e027b939b62952db4e1dc55
grilona <ilonagr0.2@gmail.com>
2023-05-22 09:15:28 -0400

    Merge with cuckoo api

---
+++ cuckooAPI/api.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file found in the request!', 400
+
+    file = request.files['file']
+
+    # Process the file or save it to a desired location
+    # Here, we are simply printing the file name
+    print('Received file:', file.filename)
+
+    return 'File uploaded successfully!'
+
+if __name__ == '__main__':
+    app.run()
